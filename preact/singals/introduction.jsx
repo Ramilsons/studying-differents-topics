0e503905c9eddd0036dea2a64f58738a41cf687a
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-02-29 18:25:35 -0300

    feat(preact): add signals introduction

---
+++ preact/singals/introduction.jsx
@@ -0,0 +1,38 @@
+/*
+    It is a method to control one value on client side using Preact
+
+    Is similar to React's useState 
+*/
+
+/*
+    Rules
+*/
+
+/* 1) Export Signal by Preact */
+const { signal } from "@preact/signals";
+
+/* 2) Create a variable with default value */
+const count = signal(0); // in this case, default value is 0
+
+/* 
+    3) We can't create this variable inside of component. Always out
+    of component 
+    
+    because always re-render the default value is apply and our count would be
+    0 (never change)
+*/
+
+
+/* To access value {variable}.value */
+console.log('This is my signal value: ' + count.value);
+
+/* To change value  {variable}.value = {newValue} */
+count.value = 2;
+
+/* To change another value based on our signal computed(() => {  }) */
+const double = computed(() => count.value * 2 ); // every count change, computed run and execute this line
+
+/* To execute a function on every change effect(()) */
+effect(() => {'My signal was updated: ' + count.value}) // run every signal change
+    
+    // The difference between Effect() and Computed() is: computed return a new signal and effect don't return
